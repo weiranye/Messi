9144f66591c7ec7269b97435189f4d719410bce6
jcw007 <jjcwang@gmail.com>
2013-06-07 10:05:11 -0700

    Added some views.

---
+++ Messi/Messi/Controllers/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Messi.Controllers
+{
+    public class GameController : Controller
+    {
+        //
+        // GET: /Game/
+
+        public ActionResult Index()
+        {
+            return View("~/views/home/listenrecord.cshtml");
+        }
+
+    }
+}
